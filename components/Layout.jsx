dd979a55866cf1b16e291f72b3785b73ac4207f5
Ananda Burger <aburger.ananda@gmail.com>
2022-04-17 19:19:10 -0300

    Add polling feature

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,19 @@
 import NavigationBar from './NavigationBar'
+import styles from './Layout.module.css'
+import * as phonesSlice from '../store/slices/phonesSlice'
+import {useSelector} from 'react-redux'
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
+  const isLoading = useSelector(phonesSlice.selectIsLoading)
+
   return (
     <div>
       <NavigationBar />
+      {isLoading &&
+       <span className={styles.spinner}>
+          <span className="spinner-border text-primary" role="status" aria-hidden="true"></span>
+        </span>
+      }
       <div className='container-lg mt-3'>
         {children}
       </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,6 @@ import Layout from '../components/Layout'
 export default function Home() {
   const phones = useSelector(phonesSlice.selectPhones)
   const nPages = useSelector(phonesSlice.selectNumberOfPages)
-  const isLoading = useSelector(phonesSlice.selectIsLoading)
   const dispatch = useDispatch()
   const router = useRouter()
   const {isReady, query} = router
@@ -18,6 +17,13 @@ export default function Home() {
   useEffect(() => {
     if (isReady) {
       dispatch(phonesSlice.fetch({page}))
+      const intervalId = setInterval(() => {
+        console.log('fetching...', {page})
+        dispatch(phonesSlice.fetch({page}))
+      }, 10000)
+      return function cleanup () {
+        clearInterval(intervalId)
+      }
     }
   }, [isReady, page])
 
@@ -25,55 +31,49 @@ export default function Home() {
     <>
       <Layout>
         <div className='d-flex justify-content-between'>
-          <h1 className='display-6'>Numbers for sale</h1>
+          <h1 className='display-6'>
+            Numbers for sale
+          </h1>
           <Link href="/add">
             <button className='btn btn-primary'>New</button>
           </Link>
         </div>
-        {isLoading ? (
-          <div className='w-100 d-flex justify-content-center mt-5'>
-            <span className="spinner-border text-primary" role="status" aria-hidden="true"></span>
-          </div>
-        ) : (
-          <table className='mt-3 table table-striped table-borderless table-hover table=responsive'>
-            <thead className="thead-light">
-              <tr>
-                <th>Phone Number</th>
-                <th>Monthly Price</th>
-                <th>Setup Price</th>
-                <th>Currency</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <>
-              <tbody>
-                {phones.map(phone => {
-                  return (
-                    <tr key={phone.id}>
-                      <td>{phone.value}</td>
-                      <td>{phone.monthlyPrice}</td>
-                      <td>{phone.setupPrice}</td>
-                      <td>{phone.currency}</td>
-                      <td>
-                        <Link href={`/edit/${phone.id}`}>
-                          <a className='link-primary'>Edit</a>
-                        </Link>
-                      </td>
-                      <td>
-                        <a className="link-primary" onClick={() => dispatch(phonesSlice.remove(phone.id))}>
-                          Delete
-                        </a>
-                      </td>
-                    </tr>
-                  )
-                })}
-              </tbody>
-            </>
-          </table>
-        )}
+        <table className='mt-3 table table-striped table-borderless table-hover table=responsive'>
+          <thead className="thead-light">
+            <tr>
+              <th>Phone Number</th>
+              <th>Monthly Price</th>
+              <th>Setup Price</th>
+              <th>Currency</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead >
+          <tbody>
+            {phones.map(phone => {
+              return (
+                <tr key={phone.id}>
+                  <td>{phone.value}</td>
+                  <td>{phone.monthlyPrice}</td>
+                  <td>{phone.setupPrice}</td>
+                  <td>{phone.currency}</td>
+                  <td>
+                    <Link href={`/edit/${phone.id}`}>
+                      <a className='link-primary'>Edit</a>
+                    </Link>
+                  </td>
+                  <td>
+                    <a className="link-primary" onClick={() => dispatch(phonesSlice.remove(phone.id))}>
+                      Delete
+                    </a>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table >
         <Pagination />
-      </Layout>
+      </Layout >
     </>
   )
 }
