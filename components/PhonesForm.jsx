a622a64dd68f8a8706b3f33ee65c6b763d7cd238
Ananda Burger <aburger.ananda@gmail.com>
2022-04-18 22:52:41 -0300

    Refactor: extract field components

--- components/PhonesForm.jsx
+++ components/PhonesForm.jsx
@@ -8,6 +8,126 @@ import { FORM_ERROR } from 'final-form'
 
 const ignore = () => {}
 
+const required = (value) => (value ? undefined : 'Required field')
+const mustBeNumber = (value) => (isNaN(value) ? 'Price must be a number' : undefined)
+
+const composeValidators =
+  (...validators) =>
+  (value) =>
+    validators.reduce((error, validator) => error || validator(value), undefined)
+
+const phoneNumberField = () => {
+  return (
+    <div className='mt-3 mb-4 w-50'>
+      <Field
+        name='value'
+        component='input'
+        type='text'
+        placeholder='Phone number'
+        validate={required}
+      >
+        {({ input, meta }) => (
+          <div>
+            <label htmlFor='value'>Phone number</label>
+            <input
+              {...input}
+              id='value'
+              type='text'
+              placeholder='Phone number'
+              className={meta.touched && meta.error ? 'form-control is-invalid' : 'form-control'}
+            />
+            {meta.error && meta.touched && <span className='invalid-feedback'>{meta.error}</span>}
+          </div>
+        )}
+      </Field>
+    </div>
+  )
+}
+
+const monthlyPriceField = () => {
+  return (
+    <div className='mb-4 w-50'>
+      <Field
+        name='monthyPrice'
+        component='input'
+        type='text'
+        placeholder='Monthly Price'
+        validate={composeValidators(required, mustBeNumber)}
+      >
+        {({ input, meta }) => (
+          <div>
+            <label htmlFor='monthlyPrice'>Monthly Price</label>
+            <input
+              {...input}
+              id='monthlyPrice'
+              type='text'
+              placeholder='0.00'
+              className={meta.touched && meta.error ? 'form-control is-invalid' : 'form-control'}
+            />
+            {meta.error && meta.touched && <span className='invalid-feedback'>{meta.error}</span>}
+          </div>
+        )}
+      </Field>
+    </div>
+  )
+}
+
+const setupPriceField = () => {
+  return (
+    <div className='mb-4 w-50'>
+      <Field
+        name='setupPrice'
+        component='input'
+        type='text'
+        placeholder='Setup Price'
+        validate={composeValidators(required, mustBeNumber)}
+      >
+        {({ input, meta }) => (
+          <div>
+            <label htmlFor='setupPrice'>Setup Price</label>
+            <input
+              {...input}
+              id='setupPrice'
+              type='text'
+              placeholder='0.00'
+              className={meta.touched && meta.error ? 'form-control is-invalid' : 'form-control'}
+            />
+            {meta.error && meta.touched && <span className='invalid-feedback'>{meta.error}</span>}
+          </div>
+        )}
+      </Field>
+    </div>
+  )
+}
+
+const currencyField = () => {
+  return (
+    <div className='mb-4 w-50'>
+      <Field
+        name='currency'
+        component='input'
+        type='text'
+        placeholder='Price currency'
+        validate={required}
+      >
+        {({ input, meta }) => (
+          <div>
+            <label htmlFor='currency'>Currency</label>
+            <input
+              {...input}
+              id='currency'
+              type='text'
+              placeholder='U$'
+              className={meta.touched && meta.error ? 'form-control is-invalid' : 'form-control'}
+            />
+            {meta.error && meta.touched && <span className='invalid-feedback'>{meta.error}</span>}
+          </div>
+        )}
+      </Field>
+    </div>
+  )
+}
+
 export default function PhonesForm({ formInitialValues, action, title }) {
   const currentPhone = useSelector(phonesSlice.selectCurrentPhone)
   const router = useRouter()
@@ -32,13 +152,6 @@ export default function PhonesForm({ formInitialValues, action, title }) {
       return formInitialValues
     }
   }
-
-  const required = (value) => (value ? undefined : 'Required field')
-  const mustBeNumber = (value) => (isNaN(value) ? 'Price must be a number' : undefined)
-  const composeValidators =
-    (...validators) =>
-    (value) =>
-      validators.reduce((error, validator) => error || validator(value), undefined)
 
   useEffect(() => {
     if (router.isReady && capitalizedTitle === 'Edit' && !formInitialValues) {
@@ -69,114 +182,10 @@ export default function PhonesForm({ formInitialValues, action, title }) {
               )}
 
               <form onSubmit={form.handleSubmit}>
-                <div className='mt-3 mb-4 w-50'>
-                  <Field
-                    name='value'
-                    component='input'
-                    type='text'
-                    placeholder='Phone number'
-                    validate={required}
-                  >
-                    {({ input, meta }) => (
-                      <div>
-                        <label htmlFor='value'>Phone number</label>
-                        <input
-                          {...input}
-                          id='value'
-                          type='text'
-                          placeholder='Phone number'
-                          className={
-                            meta.touched && meta.error ? 'form-control is-invalid' : 'form-control'
-                          }
-                        />
-                        {meta.error && meta.touched && (
-                          <span className='invalid-feedback'>{meta.error}</span>
-                        )}
-                      </div>
-                    )}
-                  </Field>
-                </div>
-                <div className='mb-4 w-50'>
-                  <Field
-                    name='monthyPrice'
-                    component='input'
-                    type='text'
-                    placeholder='Monthly Price'
-                    validate={composeValidators(required, mustBeNumber)}
-                  >
-                    {({ input, meta }) => (
-                      <div>
-                        <label htmlFor='monthlyPrice'>Monthly Price</label>
-                        <input
-                          {...input}
-                          id='monthlyPrice'
-                          type='text'
-                          placeholder='0.00'
-                          className={
-                            meta.touched && meta.error ? 'form-control is-invalid' : 'form-control'
-                          }
-                        />
-                        {meta.error && meta.touched && (
-                          <span className='invalid-feedback'>{meta.error}</span>
-                        )}
-                      </div>
-                    )}
-                  </Field>
-                </div>
-                <div className='mb-4 w-50'>
-                  <Field
-                    name='setupPrice'
-                    component='input'
-                    type='text'
-                    placeholder='Setup Price'
-                    validate={composeValidators(required, mustBeNumber)}
-                  >
-                    {({ input, meta }) => (
-                      <div>
-                        <label htmlFor='setupPrice'>Setup Price</label>
-                        <input
-                          {...input}
-                          id='setupPrice'
-                          type='text'
-                          placeholder='0.00'
-                          className={
-                            meta.touched && meta.error ? 'form-control is-invalid' : 'form-control'
-                          }
-                        />
-                        {meta.error && meta.touched && (
-                          <span className='invalid-feedback'>{meta.error}</span>
-                        )}
-                      </div>
-                    )}
-                  </Field>
-                </div>
-                <div className='mb-4 w-50'>
-                  <Field
-                    name='currency'
-                    component='input'
-                    type='text'
-                    placeholder='Price currency'
-                    validate={required}
-                  >
-                    {({ input, meta }) => (
-                      <div>
-                        <label htmlFor='currency'>Currency</label>
-                        <input
-                          {...input}
-                          id='currency'
-                          type='text'
-                          placeholder='U$'
-                          className={
-                            meta.touched && meta.error ? 'form-control is-invalid' : 'form-control'
-                          }
-                        />
-                        {meta.error && meta.touched && (
-                          <span className='invalid-feedback'>{meta.error}</span>
-                        )}
-                      </div>
-                    )}
-                  </Field>
-                </div>
+                {phoneNumberField()}
+                {monthlyPriceField()}
+                {setupPriceField()}
+                {currencyField()}
                 <div className='d-flex justify-content-between w-50'>
                   {form.submitting ? (
                     <button className='btn btn-primary' type='button' disabled>
