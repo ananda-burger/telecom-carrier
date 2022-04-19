a3d3294d50bd58764a757d1ca004cc4af81d5e47
Ananda Burger <aburger.ananda@gmail.com>
2022-04-19 16:51:38 -0300

    Hide next and previous button on small screens

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
 
 export const pagesRange = ({ page, nPages }) => {
-  const offset = 5
+  const offset = 3
   const start = Math.max(1, Math.min(page, nPages) - offset)
   const end = Math.min(nPages, Math.max(2 * offset, page + offset))
   return range(start, end + 1)
@@ -16,7 +16,7 @@ const previousLink = ({ isReady, page }) => {
   const isDisabled = !isReady || !hasPreviousPage
   const previousPage = Math.max(page - 1, 1)
   return (
-    <li className={`page-item ${isDisabled && 'disabled'}`}>
+    <li className={`page-item d-none d-sm-flex ${isDisabled && 'disabled'}`}>
       <Link href={{ pathname: '/', query: { page: previousPage } }}>
         <a className='page-link'>Previous</a>
       </Link>
@@ -28,7 +28,7 @@ const nextLink = ({ isReady, page, nPages }) => {
   const hasNextPage = page < nPages
   const isDisabled = !isReady || !hasNextPage
   return (
-    <li className={`page-item ${isDisabled && 'disabled'}`}>
+    <li className={`page-item d-none d-sm-flex ${isDisabled && 'disabled'}`}>
       <Link href={{ pathname: '/', query: { page: page + 1 } }}>
         <a className='page-link'>Next</a>
       </Link>
