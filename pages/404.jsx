5a34efdbbb3f6a077fcf028ddb2cbe51873cdeda
Ananda Burger <aburger.ananda@gmail.com>
2022-04-19 16:52:01 -0300

    Add 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <div className='d-flex flex-column text-align-middle align-items-center'>
+      <p className='display-4 mt-5'>Whoops!</p>
+      <p className='text-center my-4 fs-5'>We could not find the page you are looking for</p>
+      <Link href='/'>
+        <button className='btn btn-lg btn-primary fs-5'>Return Home</button>
+      </Link>
+    </div>
+  )
+}
